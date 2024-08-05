37fcc2b635417787207ab4642ddcd59a868289b9
Newtonfn27 <newtonfn27@gmail.com>
2024-08-05 10:42:23 -0300

    Adcidionando Projeto

---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+
+const Main = {
+
+    init: function(){
+        this.cacheSelectors()
+        this.bindEvents()
+        
+    },
+
+    cacheSelectors: function(){
+        this.checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+
+    },
+
+    bindEvents: function() {
+        const self = this
+      
+        this.checkButtons.forEach(function(button){
+        button.onclick = self.Events.checkButton_click
+       
+        })
+
+        
+        this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+    }
+,
+   Events :{
+            checkButton_click:function(e){
+                const li = e.target.parentElement
+                const isDone = li.classList.contains('done') 
+                
+                if (!isDone) {
+                    li.classList.add('done')
+
+                    return
+                    
+                    }
+                    li.classList.remove('done')
+                }
+                ,
+            
+            inputTask_keypress:function(e){
+                const key = e.key
+                const value = e.target.value
+
+                if(key==='Enter'){
+                    this.$list.innerHTML += 
+                    `<li>
+                        <div class='check'></div>
+                        <label class='task'>${value}</label>
+                        <button class='remover'></button>
+
+                    </li>
+                    `
+
+                    
+                }
+
+               S
+            }
+                
+
+       
+        
+    }
+
+}
+
+Main.init()
